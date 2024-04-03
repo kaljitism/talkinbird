42156297221a39bd6f0046a7902c8cfe768b1413
Aditya Dwivedi <dwikaljit@gmail.com>
2024-04-03 06:53:16 +0530

    Wrote configs for the UserDetail UI

--- talkinbird_flutter/lib/provider/user_provider.dart
+++ talkinbird_flutter/lib/provider/user_provider.dart
@@ -9,14 +9,40 @@ class UserProvider extends ChangeNotifier {
   UserProvider() {
     _getUserData();
     _getGenderMenu();
-    print("init");
+    _getMBTIMenu();
+    _getEnneagramMenu();
+    _getZodiacMenu();
+    _getReligionMenu();
+    _getPhilosophicalBeliefsMenu();
+    _getPoliticalAffiliationMenu();
+    _getRelationshipStatusMenu();
+    _getSexualOrientationMenu();
+    _getRomanticOrientationMenu();
+    _getEducationMenu();
+    _getProfessionMenu();
   }
 
   late User? _user;
   Map? userData;
-  final List<DropdownMenuItem> _genders = [];
+  final List<DropdownMenuItem> genders = [];
+  final List<DropdownMenuItem> mbtis = [];
+  final List<DropdownMenuItem> enneagram = [];
+  final List<DropdownMenuItem> zodiac = [];
+  final List<DropdownMenuItem> religion = [];
+  final List<DropdownMenuItem> philosophicalBeliefs = [];
+  final List<DropdownMenuItem> politicalAffiliation = [];
+  final List<DropdownMenuItem> relationshipStatus = [];
+  final List<DropdownMenuItem> sexualOrientation = [];
+  final List<DropdownMenuItem> romanticOrientation = [];
+  final List<DropdownMenuItem> education = [];
+  final List<DropdownMenuItem> profession = [];
 
-  List<DropdownMenuItem> get genderDropDownList => _genders;
+  List? getDropDownList(String key) {
+    if (key == 'gender') {
+      return genders;
+    }
+    return null;
+  }
 
   Future<void> _getUserData() async {
     try {
@@ -42,6 +68,118 @@ class UserProvider extends ChangeNotifier {
   void _getGenderMenu() {
     Gender.values.asMap().forEach((_, Gender value) {
       _genders.add(DropdownMenuItem(
+        value: value,
+        child: Text(value.toString()),
+      ));
+    });
+    notifyListeners();
+  }
+
+  void _getMBTIMenu() {
+    MBTI.values.asMap().forEach((_, MBTI value) {
+      _mbtis.add(DropdownMenuItem(
+        value: value,
+        child: Text(value.toString()),
+      ));
+    });
+    notifyListeners();
+  }
+
+  void _getEnneagramMenu() {
+    Enneagram.values.asMap().forEach((_, Enneagram value) {
+      _enneagram.add(DropdownMenuItem(
+        value: value,
+        child: Text(value.toString()),
+      ));
+    });
+    notifyListeners();
+  }
+
+  void _getZodiacMenu() {
+    Zodiac.values.asMap().forEach((_, Zodiac value) {
+      _zodiac.add(DropdownMenuItem(
+        value: value,
+        child: Text(value.toString()),
+      ));
+    });
+    notifyListeners();
+  }
+
+  void _getReligionMenu() {
+    Religion.values.asMap().forEach((_, Religion value) {
+      _religion.add(DropdownMenuItem(
+        value: value,
+        child: Text(value.toString()),
+      ));
+    });
+    notifyListeners();
+  }
+
+  void _getPhilosophicalBeliefsMenu() {
+    Philosophy.values.asMap().forEach((_, Philosophy value) {
+      _philosophicalBeliefs.add(DropdownMenuItem(
+        value: value,
+        child: Text(value.toString()),
+      ));
+    });
+    notifyListeners();
+  }
+
+  void _getPoliticalAffiliationMenu() {
+    PoliticalAffiliation.values
+        .asMap()
+        .forEach((_, PoliticalAffiliation value) {
+      _politicalAffiliation.add(DropdownMenuItem(
+        value: value,
+        child: Text(value.toString()),
+      ));
+    });
+    notifyListeners();
+  }
+
+  void _getRelationshipStatusMenu() {
+    RelationshipStatus.values.asMap().forEach((_, RelationshipStatus value) {
+      _relationshipStatus.add(DropdownMenuItem(
+        value: value,
+        child: Text(value.toString()),
+      ));
+    });
+    notifyListeners();
+  }
+
+  void _getSexualOrientationMenu() {
+    SexualOrientation.values.asMap().forEach((_, SexualOrientation value) {
+      _sexualOrientation.add(DropdownMenuItem(
+        value: value,
+        child: Text(value.toString()),
+      ));
+    });
+    notifyListeners();
+  }
+
+  void _getRomanticOrientationMenu() {
+    RomanticOrientation.values.asMap().forEach((_, RomanticOrientation value) {
+      _romanticOrientation.add(DropdownMenuItem(
+        value: value,
+        child: Text(value.toString()),
+      ));
+    });
+    notifyListeners();
+  }
+
+  void _getEducationMenu() {
+    Education.values.asMap().forEach((_, Education value) {
+      _education.add(DropdownMenuItem(
+        value: value,
+        child: Text(value.toString()),
+      ));
+    });
+    notifyListeners();
+  }
+
+  void _getProfessionMenu() {
+    Profession.values.asMap().forEach((_, Profession value) {
+      _profession.add(DropdownMenuItem(
         value: value,
         child: Text(value.toString()),
       ));
--- talkinbird_flutter/lib/screens/screens/user_details_ui.dart
+++ talkinbird_flutter/lib/screens/screens/user_details_ui.dart
@@ -14,31 +14,157 @@ class _UserDetailsUIState extends State<UserDetailsUI> {
   late TextEditingController nameController;
   late TextEditingController emailController;
   late TextEditingController ageController;
+  late TextEditingController phoneNumberController;
+  late TextEditingController cityController;
+  late TextEditingController countryController;
+  late TextEditingController dateOfBirthController;
+  late TextEditingController genderController;
+  late TextEditingController bioController;
+  late TextEditingController profilePictureController;
+  late TextEditingController mbtiController;
+  late TextEditingController enneagramController;
+  late TextEditingController zodiacController;
+  late TextEditingController religionController;
+  late TextEditingController philosophicalBeliefsController;
+  late TextEditingController politicalAffiliationController;
+  late TextEditingController relationshipStatusController;
+  late TextEditingController sexualOrientationController;
+  late TextEditingController romanticOrientationController;
+  late TextEditingController educationController;
+  late TextEditingController professionController;
+  late TextEditingController interestsController;
+  late TextEditingController hobbiesController;
+  late TextEditingController languagesController;
+  late TextEditingController skillsController;
+  late TextEditingController musicController;
+  late TextEditingController moviesController;
+  late TextEditingController tvShowsController;
+  late TextEditingController booksController;
+  late TextEditingController podcastsController;
+  late TextEditingController gamesController;
+  late TextEditingController sportsController;
+  late TextEditingController placesController;
+  late TextEditingController foodsController;
+  late TextEditingController drinksController;
+  late TextEditingController animalsController;
+  late TextEditingController countriesVisitedController;
+  late TextEditingController letterLengthController;
+  late TextEditingController letterFrequencyController;
+  late TextEditingController replyTimeController;
+  late TextEditingController targetGenderController;
+  late TextEditingController targetAgeController;
+  late TextEditingController targetCountryController;
+  late TextEditingController targetMBTIController;
+  late TextEditingController targetEnneagramController;
+  late TextEditingController targetZodiacController;
+  late TextEditingController targetReligionController;
+  late TextEditingController targetPhilosophicalBeliefsController;
+  late TextEditingController targetPoliticalAffiliationController;
+  late TextEditingController targetRelationshipStatusController;
+  late TextEditingController targetSexualOrientationController;
+  late TextEditingController targetRomanticOrientationController;
+  late TextEditingController targetEducationController;
+  late TextEditingController targetProfessionController;
+  late TextEditingController targetInterestsController;
+  late TextEditingController targetHobbiesController;
+  late TextEditingController targetLanguagesController;
+  late TextEditingController targetSkillsController;
+  late TextEditingController targetMusicController;
+  late TextEditingController targetMoviesController;
+  late TextEditingController targetTVShowsController;
+  late TextEditingController targetBooksController;
+  late TextEditingController targetPodcastsController;
+  late TextEditingController targetGamesController;
+  late TextEditingController targetSportsController;
+  late TextEditingController targetPlacesController;
+  late TextEditingController targetFoodsController;
+  late TextEditingController targetDrinksController;
+  late TextEditingController targetAnimalsController;
+  late TextEditingController targetCountriesVisitedController;
+  late TextEditingController targetLetterLengthController;
+  late TextEditingController targetLetterFrequencyController;
+  late TextEditingController targetReplyTimeController;
+  late TextEditingController excludeGenderController;
+  late TextEditingController excludeAgeController;
+  late TextEditingController excludeCountryController;
+  late TextEditingController excludeMBTIController;
+  late TextEditingController excludeEnneagramController;
+  late TextEditingController excludeZodiacController;
+  late TextEditingController excludeReligionController;
+  late TextEditingController excludePhilosophicalBeliefsController;
+  late TextEditingController excludePoliticalAffiliationController;
+  late TextEditingController excludeRelationshipStatusController;
+  late TextEditingController excludeSexualOrientationController;
+  late TextEditingController excludeRomanticOrientationController;
+  late TextEditingController excludeEducationController;
+  late TextEditingController excludeProfessionController;
+  late TextEditingController excludeInterestsController;
+  late TextEditingController excludeHobbiesController;
+  late TextEditingController excludeLanguagesController;
+  late TextEditingController excludeSkillsController;
+  late TextEditingController excludeMusicController;
+  late TextEditingController excludeMoviesController;
+  late TextEditingController excludeTVShowsController;
+  late TextEditingController excludeBooksController;
+  late TextEditingController excludePodcastsController;
+  late TextEditingController excludeGamesController;
+  late TextEditingController excludeSportsController;
+  late TextEditingController excludePlacesController;
+  late TextEditingController excludeFoodsController;
+  late TextEditingController excludeDrinksController;
+  late TextEditingController excludeAnimalsController;
+  late TextEditingController excludeCountriesVisitedController;
+  late TextEditingController excludeLetterLengthController;
+  late TextEditingController excludeLetterFrequencyController;
+  late TextEditingController excludeReplyTimeController;
+
   late List<TextEditingController> controllers;
 
   late Map userData;
   List<Widget> form = [];
 
   Future<void> loadData() async {
+    // Creating FormFields
     var user = await client.user.getUser(uuid);
     var userRow = user[0];
     userData = userRow.toJson();
     int index = 0;
     userData.forEach((key, value) {
-      // var controller =  controllers[index];
-      form.add(
-        TextFormField(
-          // controller: controller,
-          initialValue: value.toString(),
-          onChanged: (value) {
-            // controller.text = value;
-          },
-          decoration: InputDecoration(
-            labelText: key,
+      if (key != 'gender' || key != 'mbti' || key != 'enneagram' || key != 'zodiac' || key != 'religion' || key != 'philosophicalBeliefs' || key != 'politicalAffiliation' || key != 'relationshipStatus' || key != 'sexualOrientation' || key != 'romanticOrientation' || key != 'education' || key != 'profession' || key != 'uuid') {
+        var controller = controllers[index];
+        form.add(
+          TextFormField(
+            controller: controller,
+            initialValue: value.toString(),
+            onChanged: (value) {
+              controller.text = value;
+            },
+            decoration: InputDecoration(
+              labelText: key,
+            ),
           ),
-        ),
-      );
-      index++;
+        );
+        index++;
+      }
+
+      // Creating Dropdown Form Fields
+      else {
+        form.add(
+          DropdownButton(
+            isExpanded: true,
+            menuMaxHeight: 300,
+            borderRadius: BorderRadius.circular(20),
+            hint: Text(key),
+            value: value,
+            onChanged: (newValue) {
+              setState(() {
+                // selectedValue = newValue!;
+              });
+            },
+            items: ,
+          ),
+        );
+      }
     });
   }
 
@@ -47,6 +173,32 @@ class _UserDetailsUIState extends State<UserDetailsUI> {
     nameController = TextEditingController();
     emailController = TextEditingController();
     ageController = TextEditingController();
+    genderController = TextEditingController();
+    phoneNumberController = TextEditingController();
+    cityController = TextEditingController();
+    countryController = TextEditingController();
+    dateOfBirthController = TextEditingController();
+    bioController = TextEditingController();
+    profilePictureController = TextEditingController();
+    mbtiController = TextEditingController();
+    enneagramController = TextEditingController();
+    zodiacController = TextEditingController();
+    religionController = TextEditingController();
+    philosophicalBeliefsController = TextEditingController();
+    politicalAffiliationController = TextEditingController();
+    relationshipStatusController = TextEditingController();
+    sexualOrientationController = TextEditingController();
+    romanticOrientationController = TextEditingController();
+    educationController = TextEditingController();
+    professionController = TextEditingController();
+    interestsController = TextEditingController();
+    hobbiesController = TextEditingController();
+    languagesController = TextEditingController();
+    skillsController = TextEditingController();
+    musicController = TextEditingController();
+    moviesController = TextEditingController();
+    tvShowsController = TextEditingController();
+
   }
 
   @override
